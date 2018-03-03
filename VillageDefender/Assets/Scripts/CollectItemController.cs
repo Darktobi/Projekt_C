d58ca0e4ac810a3ea0fc97cf59f5225b18b9db11
DESKTOP-NG9UFGU\Tobi_ <tobi_360@yahoo.de>
2018-03-03 16:01:50 +0100

    Created first prototype where Player (cube) can collect items (sphere) Items spawn randomly in a specific area

---
+++ VillageDefender/Assets/Scripts/CollectItemController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectItemController : MonoBehaviour {
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            Debug.Log("Item eingesammelt");
+            Destroy(gameObject);
+        }
+    }
+
+}
---
+++ VillageDefender/Assets/Scripts/SpawnCollectable.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnCollectable : MonoBehaviour {
+
+    public GameObject item;
+    public float interval = 2;
+
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("spawnItem", interval, interval);
+	}
+	
+
+    void spawnItem()
+    {
+        System.Random rnd = new System.Random();
+        int xValue = rnd.Next(-10, 11);
+        int yValue = rnd.Next(-5, 6);
+        GameObject g = (GameObject)Instantiate(item, new Vector3(xValue, yValue), Quaternion.identity);
+    }
+}
