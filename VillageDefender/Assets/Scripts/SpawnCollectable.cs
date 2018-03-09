4bf55e914e7eacca81c80243fbb1a7ee672fc7fd
DESKTOP-NG9UFGU\Tobi_ <tobi_360@yahoo.de>
2018-03-09 12:28:07 +0100

    Added an object which is able to spawn a maximum number of other objects when it is destroyed.

--- VillageDefender/Assets/Scripts/SpawnCollectable.cs
+++ VillageDefender/Assets/Scripts/SpawnCollectable.cs
@@ -4,20 +4,47 @@ using UnityEngine;
 
 public class SpawnCollectable : MonoBehaviour {
 
-    public GameObject item;
+    public GameObject spawnableItem;
     public float interval = 2;
 
-	// Use this for initialization
-	void Start () {
-        InvokeRepeating("spawnItem", interval, interval);
-	}
-	
+    public int life = 3;
+    public int maxNumberOfItems = 5;
+
+
+    private void FixedUpdate()
+    {
+        if (life <= 0)
+        {
+            spawnItem();
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
+                life--;
+            }
+        }
+    }
 
     void spawnItem()
     {
+        
         System.Random rnd = new System.Random();
-        int xValue = rnd.Next(-10, 11);
-        int yValue = rnd.Next(-5, 6);
-        GameObject g = (GameObject)Instantiate(item, new Vector3(xValue, yValue), Quaternion.identity);
+        int numberOfItems = rnd.Next(maxNumberOfItems + 1);
+        int xPosition = (int)GetComponent<Transform>().position.x;
+        int yPosition = (int)GetComponent<Transform>().position.y;
+
+        for(int i = 0; i<=numberOfItems; i++)
+        {
+            int xValue = rnd.Next(xPosition -2, xPosition +2);
+            int yValue = rnd.Next(yPosition -2, yPosition +2);
+            GameObject g = (GameObject)Instantiate(spawnableItem, new Vector3(xValue, yValue), Quaternion.identity);
+        }
+
     }
 }
