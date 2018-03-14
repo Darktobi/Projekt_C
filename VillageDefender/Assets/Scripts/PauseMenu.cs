0ba52205078f3ac65dab5c3f38a07e885468da1f
Chuchu_chan-PC\Chuchu_chan <isabell-jennifer94@web.de>
2018-03-14 10:59:45 +0100

    Pause Menü ins Spiel eingebaut

---
+++ VillageDefender/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+	public Canvas canvasPause;
+	// Use this for initialization
+	void Start () {
+		GoOnFunktions();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Pause();
+			canvasPause.gameObject.SetActive(true);
+        }
+	}
+	
+	public void Pause()
+    {	
+		Cursor.lockState = CursorLockMode.None; 
+        Cursor.visible = true;
+        Time.timeScale = 0;
+    }
+	public void GoOnFunktions()
+	{
+		Cursor.lockState = CursorLockMode.Locked; 
+        Cursor.visible = false;
+        canvasPause.gameObject.SetActive(false);
+        Time.timeScale = 1;
+	}
+		public void Quit()
+    {
+	#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+	#else
+        Application.Quit ();
+	#endif
+    }
+	public void To_Main_Menu()
+    {
+        SceneManager.LoadScene("Hauptmenu");
+    }
+	
+}
