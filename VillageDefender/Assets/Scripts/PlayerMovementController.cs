5d05fd0f8d7c5a1359e264e002da25e1393ac448
DESKTOP-NG9UFGU\Tobi_ <tobi_360@yahoo.de>
2018-03-16 17:36:02 +0100

    Combine "PlayerMovement" and "PlayerMovementController" and delete "PlayerMovement" Added Player to Prototype Scene and as Prefab

--- VillageDefender/Assets/Scripts/PlayerMovementController.cs
+++ VillageDefender/Assets/Scripts/PlayerMovementController.cs
@@ -6,12 +6,41 @@ public class PlayerMovementController : MonoBehaviour {
 
     public float speed = 10;
 
+    Rigidbody2D rbody;
+    Animator anim;
+
+    void Start()
+    {
+
+        rbody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
+
+    }
+
     private void FixedUpdate()
     {
+        
         float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
 
         Vector2 dir = new Vector2(h, v);
-        GetComponent<Rigidbody2D>().velocity = dir.normalized * speed;
+     
+        if (dir != Vector2.zero)
+        {
+
+            anim.SetBool("is walking", true);
+            anim.SetFloat("input_x", dir.x);
+            anim.SetFloat("input_y", dir.y);
+
+        }
+        else
+        {
+
+            anim.SetBool("is walking", false);
+        }
+
+        rbody.velocity = dir.normalized * speed;
+
     }
 }
