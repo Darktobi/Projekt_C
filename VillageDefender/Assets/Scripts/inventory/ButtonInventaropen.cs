865d76e7244998dd42548cb73256d6a547f25dd4
nemotie <marcoboesling@hotmail.com>
2018-04-03 20:32:45 +0200

    dorf2 Inventar auf Basis von Tobis UI

---
+++ VillageDefender/Assets/Scripts/inventory/ButtonInventaropen.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonInventaropen : MonoBehaviour {
+
+	public Animator animator;
+
+
+	public void OpenCloseInventory ()
+	{
+		if (animator.GetBool ("isOpen")) {
+			animator.SetBool ("isOpen", false);
+		} 
+		else 
+		{
+			animator.SetBool ("isOpen", true);
+		}
+	}
+
+
+}
---
+++ VillageDefender/Assets/Scripts/inventory/UIInventory.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIInventory : MonoBehaviour {
+
+	private Player player;
+
+	public Image icon;
+	public Text counter;
+
+	//public Animator animator;
+
+
+	public void addinventar()
+	{
+	//	animator.SetBool("isOpen", true);
+	counter.enabled = true;
+	icon.enabled = true;
+	counter.text = player.getCollectedWood().ToString();
+
+	}
+
+	public void clearinventar()
+	{
+		// icon.sprite = null;
+		icon.enabled = false;
+		counter.enabled = false;
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+	}
+
+
+	
+	// Update is called once per frame
+	void Update () {
+		if (player.getCollectedWood () > 0) {
+
+			addinventar ();
+			// Debug.Log ("Hallo hab Holz");
+		}
+
+
+
+	}
+}
