e794f6412b633e50633ccc5a10489817c0c68ae7
nemotie <marcoboesling@hotmail.com>
2018-04-14 20:21:13 +0200

    t

--- VillageDefender/Assets/Scripts/LoadnewArea.cs
+++ VillageDefender/Assets/Scripts/LoadnewArea.cs
@@ -1,13 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LoadnewArea : MonoBehaviour {
 
 	public string leveltoLoad;
 	public string exitPoint;
 
+
 	private PlayerMovementController thePlayer;
+
+
+
 
 	// Use this for initialization
 	void Start () {
@@ -18,12 +23,25 @@ public class LoadnewArea : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
+	
+
+
 	}
 
 	void OnTriggerEnter2D(Collider2D other) {
 
 		if (other.gameObject.name == "Player") {
 			Application.LoadLevel (leveltoLoad);
+
+			//SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+			//SceneManager.LoadScene(leveltoLoad, LoadSceneMode.Single);
+
+		
+
+
+
+
 			thePlayer.startPoint = exitPoint;
 		}
 	}
---
+++ VillageDefender/Assets/Scripts/Quests/brueckenquest.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class brueckenquest : MonoBehaviour {
+	private Player player;
+	private int holzanzahl = 0;
+
+	private GameObject bruecke;
+
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag == "Player") {
+
+			holzanzahl =  FindObjectOfType<Player> ().getCollectedWood ();
+				
+			}
+
+	
+
+
+
+		if (holzanzahl >= 5) {
+
+			// Holzbrücke anzeigen
+			bruecke = GameObject.Find ("Holzbruecke");
+			bruecke.GetComponent<Renderer>().enabled = true;
+
+					
+			Destroy (gameObject);
+
+
+
+		}
+
+	}
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
