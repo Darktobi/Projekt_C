50d25b55a91102750d35da3d0cbe567178076572
DESKTOP-NG9UFGU\Tobi_ <tobi_360@yahoo.de>
2018-04-25 17:02:10 +0200

    Created first Enemy with Animation. ToDo: Improve animation in Code.

--- VillageDefender/Assets/Scripts/EnemyMovementController.cs
+++ VillageDefender/Assets/Scripts/EnemyMovementController.cs
@@ -7,15 +7,62 @@ public class EnemyMovementController : MonoBehaviour {
     public float speed = 1;
 
     GameObject target;
+    Animator anim;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
+        anim = GetComponent<Animator>();
         target = GameObject.FindWithTag("Player");
 	}
 	
 	// Update is called once per frame
 	void Update () {
         Vector2 v2 = (target.transform.position - transform.position).normalized;
+        //Debug.Log(v2);
+
+        changeAnimation(v2);
         GetComponent<Rigidbody2D>().velocity = v2 * speed;
 	}
+
+    void changeAnimation(Vector2 v)
+    {
+        if(v.y > 0)
+        {
+            if(v.y < 0.3f)
+            {
+                if(v.x > 0)
+                {
+                    anim.SetInteger("direction", 2);
+                }
+                else if (v.x < 0)
+                {
+                    anim.SetInteger("direction", 4);
+                }
+            }
+            else
+            {
+                anim.SetInteger("direction", 1);
+            }  
+        }
+        else if(v.y < 0)
+        {
+            if (v.y > -0.3f)
+            {
+                if (v.x > 0)
+                {
+                    anim.SetInteger("direction", 2);
+                }
+                else if (v.x < 0)
+                {
+                    anim.SetInteger("direction", 4);
+                }
+            }
+            else
+            {
+                anim.SetInteger("direction", 3);
+            }
+        }
+    }
+
+
 }
