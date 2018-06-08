cfaf58ad7ea02fe66cbf957b571bf1946d5f0013
nemotie <marcoboesling@hotmail.com>
2018-06-08 12:45:22 +0200

    1

--- VillageDefender/Assets/Scripts/Ki Animation/NPCAnimationWaypoint.cs
+++ VillageDefender/Assets/Scripts/Ki Animation/NPCAnimationWaypoint.cs
@@ -9,6 +9,7 @@ public class NPCAnimationWaypoint : MonoBehaviour {
 	public GameObject waypoint_1;
 	public GameObject waypoint_2;
 
+	bool playerNotdetected = true;
 
 	GameObject destination;
 	GameObject player;
@@ -20,11 +21,8 @@ public class NPCAnimationWaypoint : MonoBehaviour {
 		rbody = GetComponent<Rigidbody2D> ();
 		anim = GetComponent<Animator> ();
 		player = GameObject.FindWithTag("Player");
-
 		destination = waypoint_1;
-
 	}
-
 
 	// Waypoint wechsel bei colision
 	void OnTriggerEnter2D(Collider2D other)
@@ -38,37 +36,48 @@ public class NPCAnimationWaypoint : MonoBehaviour {
 				destination = waypoint_1;
 			}
 		}
-
 		//wenn spieler, dann halte an
 		if (other.tag == "Player") {
-
-			destination = player;
-		}
+			playerNotdetected = false;
+			}
 	}
 
+	void OnTriggerExit2D(Collider2D other)
+	{
+		playerNotdetected = true;
+	}
 
+	private void move()
+	{
+	Vector2 v2 = (destination.transform.position - transform.position).normalized;
+	GetComponent<Rigidbody2D>().velocity = v2 * speed;
 
+	//Befehle für den Animator 
+	//Richtung
+	anim.SetFloat("input_x", v2.x);
+	anim.SetFloat("input_y", v2.y);
 
-
-
+	//animator geht oder läuft
+	if (v2 != Vector2.zero) {
+		anim.SetBool ("is walking", true);
+		} else {
+		anim.SetBool ("is walking", false);
+		}
+	}
 
 	// Update is called once per frame
 	void Update () {
-		Vector2 v2 = (destination.transform.position - transform.position).normalized;
-		GetComponent<Rigidbody2D>().velocity = v2 * speed;
+		if (playerNotdetected) {
+			//wenn Spieler, dann nicht bewegen
+			move ();
 
-
-		//Befehle für den Animator 
-		//Richtung
-		anim.SetFloat("input_x", v2.x);
-		anim.SetFloat("input_y", v2.y);
-
-		//animator geht oder läuft
-		if (v2 != Vector2.zero) {
-			anim.SetBool ("is walking", true);
 		} else {
+			//NPC steht
+			GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
 			anim.SetBool ("is walking", false);
+		
 		}
 
 	}
+
 }
