642fe878411d4296e0cc5f1af6b1f88dc39ee33e
nemotie <marcoboesling@hotmail.com>
2018-06-08 14:19:47 +0200

    Ork und Dorfbeweohner Waypoints

---
+++ VillageDefender/Assets/Scripts/Ki Animation/OrkWaypointChaseSwitch.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrkWaypointChaseSwitch : MonoBehaviour {
+
+
+
+
+
+	// Use this for initialization
+	void Start () {
+
+		GetComponent<EnemyChaseMovementController> ().enabled = false;
+
+
+	}
+
+
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+
+		if (collision.gameObject.tag == "Player")
+		{
+			GetComponent<NPCAnimationWaypoint> ().enabled = false;
+			GetComponent<EnemyChaseMovementController> ().enabled = true;
+
+		}
+	} 
+
+	
+	// Update is called once per frame
+	void Update () {
+
+	
+
+		
+	}
+}
